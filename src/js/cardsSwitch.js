e822774d4eb45d0dde09dff9b7a0012d39117394
Mateusz <wirus006@gmail.com>
2021-04-09 21:22:26 +0200

    implement cardsSwitch for references section

--- src/js/cardsSwitch.js
+++ src/js/cardsSwitch.js
@@ -3,21 +3,56 @@ const rightSwitch = document.querySelector('.realizations__switch-right--js');
 const cards = document.querySelector('.realizations__cards--js');
 
 leftSwitch.addEventListener('click', () => {
-    if(cards.classList.contains('transformLeft')) {
-        cards.classList.remove('transformLeft');
+    if(cards.classList.contains('switchRight')) {
+        cards.classList.remove('switchRight');
         rightSwitch.classList.remove('iconRightTransparent');
     } else {
-        cards.classList.add('transformRight');
+        cards.classList.add('switchLeft');
         leftSwitch.classList.add('iconLeftTransparent');
     }
 })
 
 rightSwitch.addEventListener('click', () => {
-    if(cards.classList.contains('transformRight')) {
-        cards.classList.remove('transformRight');
+    if(cards.classList.contains('switchLeft')) {
+        cards.classList.remove('switchLeft');
         leftSwitch.classList.remove('iconLeftTransparent');
     } else {
-        cards.classList.add('transformLeft');
+        cards.classList.add('switchRight');
         rightSwitch.classList.add('iconRightTransparent');
+    }
+})
+
+const leftSwitchRef = document.querySelector('.references__switch-left--js');
+const rightSwitchRef = document.querySelector('.references__switch-right--js');
+const cardsRef = document.querySelector('.references__cards--js');
+
+console.log(rightSwitchRef);
+
+leftSwitchRef.addEventListener('click', () => {
+    if(cardsRef.classList.contains('switchRight2')) {
+        cardsRef.classList.remove('switchRight2');
+        rightSwitchRef.classList.remove('iconRightTransparent');
+    } else if(cardsRef.classList.contains('switchRight')) {
+        cardsRef.classList.remove('switchRight');
+    } else if(cardsRef.classList.contains('switchLeft')) {
+        cardsRef.classList.add('switchLeft2');
+        console.log("powinno się dodać");
+        leftSwitchRef.classList.add('iconLeftTransparent');
+    } else {
+        cardsRef.classList.add('switchLeft');
+    }
+})
+
+rightSwitchRef.addEventListener('click', () => {
+    if(cardsRef.classList.contains('switchLeft2')) {
+        cardsRef.classList.remove('switchLeft2');
+        leftSwitchRef.classList.remove('iconLeftTransparent');
+    } else if(cardsRef.classList.contains('switchLeft')) {
+        cardsRef.classList.remove('switchLeft');
+    } else if(cardsRef.classList.contains('switchRight')) {
+        cardsRef.classList.add('switchRight2');
+        rightSwitchRef.classList.add('iconRightTransparent');
+    } else {
+        cardsRef.classList.add('switchRight');
     }
 })
