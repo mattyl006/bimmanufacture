079c55e1de2d855b432537051ee58a4240fde6ff
Mateusz <wirus006@gmail.com>
2021-08-20 16:39:29 +0200

    hero and navigation update

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,11 +1,13 @@
-const navButton = document.querySelector('.navigation__menu--js');
-const navList = document.querySelector('.navigation__list--js');
+const hamburger = document.querySelector('.navigation__hamburger--js');
+const menu = document.querySelector('.navigation__list--js');
 
 const plButton = document.querySelector('.navigation__polish--js');
 const engButton = document.querySelector('.navigation__english--js');
 
-navButton.addEventListener('click', () => {
-    navList.classList.toggle('visible');
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('navigation__hamburger--active');
+    menu.classList.add('menu--animation');
+    menu.classList.toggle('visible');
 });
 
 plButton.addEventListener('click', () => {
