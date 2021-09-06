143dd97206d1ea20a3056f7fed8f60bd11b83837
Mateusz <wirus006@gmail.com>
2021-09-06 16:14:16 +0200

    correct mobile nav

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -4,10 +4,24 @@ const menu = document.querySelector('.navigation__list--js');
 const plButton = document.querySelector('.navigation__polish--js');
 const engButton = document.querySelector('.navigation__english--js');
 
+const main = document.getElementById('main--js');
+
+main.addEventListener('click', () => {
+    if(menu.classList.contains('visible')) {
+        menu.classList.remove('visible');
+        hamburger.classList.remove('navigation__hamburger--active');
+    }
+})
+
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('navigation__hamburger--active');
     menu.classList.add('menu--animation');
-    menu.classList.toggle('visible');
+    if(menu.classList.contains('visible')) {
+        menu.classList.remove('visible');
+        hamburger.classList.remove('navigation__hamburger--active');
+    } else {
+        menu.classList.add('visible');
+        hamburger.classList.add('navigation__hamburger--active');
+    }
 });
 
 plButton.addEventListener('click', () => {
